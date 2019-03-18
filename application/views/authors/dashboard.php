bd6240e6ec5fe03b74d675fdaf1c530b8715610b
themaruf <hasan.m.maruf@gmail.com>
2019-03-19 00:52:35 +0600

    admin lte integration

--- application/views/authors/dashboard.php
+++ application/views/authors/dashboard.php
@@ -2,45 +2,139 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $this->load->view("partial/header");
 ?>
-    <div class="col-md-8">
-    	<div class="input-group">
-		  <div class="input-group-prepend">
-		    <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-		  </div>
-		  <div class="custom-file">
-		    <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-		    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-		  </div>
-      <br/>
-</div>
-      <table class="table table-bordered table-striped">
-        <tr>
-          <th colspan="1"><h4 class="text-center">User Info</h3></th>
-          <th>
-          	<a href="<?php echo base_url('authors/logout');?>" >  <button type="button" class="btn-primary">Logout</button></a>
 
- 		  </th>
+    <!-- Main content -->
+    <section class="content">
 
-        </tr>
-            <tr>
-            <td>Author ID</td>
-            <td><?php echo $author_info->author_id; ?></td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td><?php echo $author_info->first_name." ". $author_info->last_name; ?></td>
-          </tr>
-          <tr>
-            <td>Phone Number</td>
-            <td><?php echo $author_info->phone_number;  ?></td>
-          </tr>
-          <tr>
-            <td>Email</td>
-            <td><?php echo $author_info->email;  ?></td>
-          </tr>
-      </table>
-    </div>
+      <div class="row">
+        <div class="col-md-3">
 
+          <!-- Profile Image -->
+          <div class="box box-primary">
+            <div class="box-body box-profile">
+              <img class="profile-user-img img-responsive img-circle" src="<?php echo base_url();?>adminlte/dist/img/avatar.png" alt="User profile picture">
 
+              <h3 class="profile-username text-center"><?php echo $author_info->first_name." ". $author_info->last_name; ?></h3>
 
+              <p class="text-muted text-center">Author</p>
+
+              <ul class="list-group list-group-unbordered">
+                <li class="list-group-item">
+                  <b>Email</b> <a class="pull-right"><?php echo $author_info->email;?></a>
+                </li>
+                <li class="list-group-item">
+                  <b>Phone Number</b> <a class="pull-right"><?php echo $author_info->phone_number;?></a>
+                </li>
+                <li class="list-group-item">
+                  <b>DOB</b> <a class="pull-right"><?php echo $author_info->dob;?></a>
+                </li>
+              </ul>
+
+              <a href="<?php echo base_url('authors/logout');?>" class="btn btn-danger btn-block"><b>Logout</b></a>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+          <!-- About Me Box -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">About Me</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <strong><i class="fa fa-book margin-r-5"></i> Education</strong>
+
+              <p class="text-muted">
+                B.S. in Computer Science from the University of Tennessee at Knoxville
+              </p>
+
+              <hr>
+
+              <strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
+
+              <p class="text-muted">Malibu, California</p>
+
+<!--               <hr>
+
+              <strong><i class="fa fa-pencil margin-r-5"></i> Skills</strong>
+
+              <p>
+                <span class="label label-danger">UI Design</span>
+                <span class="label label-success">Coding</span>
+                <span class="label label-info">Javascript</span>
+                <span class="label label-warning">PHP</span>
+                <span class="label label-primary">Node.js</span>
+              </p> -->
+
+              <hr>
+
+              <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-9">
+
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Papers</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Paper ID</th>
+                  <th>Title</th>
+                  <th>Keywords</th>
+                  <th>Abstract</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Test Title</td>
+                  <td>ios android mobile</td>
+                  <td>lorem ipsum...</td>
+                  <td>On Review</td>
+                  <td><a href="#" class="modal-dlg" data-btn-submit="Submit" title="Edit"><span class="glyphicon glyphicon-edit"></span></a>  <a href="#" class="modal-dlg" title="Details"><span class="glyphicon glyphicon-list-alt"></span></a>  <a href="#" class="modal-dlg" data-btn-submit="Submit" title="Delete"><span class="glyphicon glyphicon-trash"></span></a></td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Test paper</td>
+                  <td>iot machine learning</td>
+                  <td>lorem ipsum...</td>
+                  <td>Reviewed</td>
+                  <td><a href="#" class="modal-dlg" data-btn-submit="Submit" title="Edit"><span class="glyphicon glyphicon-edit"></span></a>  <a href="#" class="modal-dlg" title="Details"><span class="glyphicon glyphicon-list-alt"></span></a>  <a href="#" class="modal-dlg" data-btn-submit="Submit" title="Delete"><span class="glyphicon glyphicon-trash"></span></a></td>
+                </tr>
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>Paper ID</th>
+                  <th>Title</th>
+                  <th>Keywords</th>
+                  <th>Abstract</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+    </section>
+    <!-- /.content -->
 
--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -2,104 +2,134 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<base href="<?php echo base_url();?>" />
 	<title></title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/bower_components/Ionicons/css/ionicons.min.css">
+   <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/dist/css/skins/_all-skins.min.css">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/bower_components/morris.js/morris.css">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/bower_components/jvectormap/jquery-jvectormap.css">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 
-	<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>images/icon.png"/>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/bootstrap.min.css"/>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/login.css"/>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/styles.css"/>
+  <link rel="stylesheet" href="<?php echo base_url();?>adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>css/styles.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>css/login.css">
 
-	<script src="<?php echo base_url();?>js/jquery-3.3.1.min.js"></script>
-	<script src="<?php echo base_url();?>js/bootstrap.min.js"></script>
-	<script src="<?php echo base_url();?>js/jquery-ui.js"></script>
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
 	<style type="text/css">
 		html {
 			overflow: auto;
 		}
 	</style>
+
+	<!-- jQuery 3 -->
+<script src="<?php echo base_url();?>adminlte/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?php echo base_url();?>adminlte/bower_components/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url();?>adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- DataTables -->
+<script src="<?php echo base_url();?>adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url();?>adminlte/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<!-- Morris.js charts -->
+<script src="<?php echo base_url();?>adminlte/bower_components/raphael/raphael.min.js"></script>
+<script src="<?php echo base_url();?>adminlte/bower_components/morris.js/morris.min.js"></script>
+<!-- Sparkline -->
+<script src="<?php echo base_url();?>adminlte/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- jvectormap -->
+<script src="<?php echo base_url();?>adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo base_url();?>adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo base_url();?>adminlte/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="<?php echo base_url();?>adminlte/bower_components/moment/min/moment.min.js"></script>
+<script src="<?php echo base_url();?>adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<!-- datepicker -->
+<script src="<?php echo base_url();?>adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="<?php echo base_url();?>adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<!-- Slimscroll -->
+<script src="<?php echo base_url();?>adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url();?>adminlte/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url();?>adminlte/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo base_url();?>adminlte/dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url();?>adminlte/dist/js/demo.js"></script>
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">CONFMAG</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo base_url('home/index');?>">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('home/about');?>">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('authors/login');?>">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('authors/signup');?>">Signup</a>
-      </li>
-<!--       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Home</a>
-          <a class="dropdown-item" href="#">About Us</a>
+<div class="container-fluid">
+    <nav class="navbar navbar-inverse">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo base_url('home/index');?>">CONFMAG</a>
         </div>
-      </li> -->
-    </ul>
-
-  </div>
-</nav>
-
-
-
-		<div class="container-fluid">
-			<div class="row">
-
-
-	<script type="text/javascript">
-		//language change button -> block CTRL+click
-        $(document).ready(function() {
-	        $('.langChange').click(function (e){  
-			    if (e.ctrlKey) {
-			        return false;
-			    }
-			    else if(e.shiftKey){
-			    	return false;
-			    }
-			});
-        });
-    </script>
+        <!-- Collection of nav links, forms, and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="<?php echo base_url('home/about');?>">About Us</a></li>
+                <li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Messages <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Inbox</a></li>
+                        <li><a href="#">Drafts</a></li>
+                        <li><a href="#">Sent Items</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Trash</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?php echo base_url('authors/login');?>">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
 
 <style type="text/css">
-    .dropdown-menu>li>a {
-	    padding: 10px 20px;
-	}	
-	.navbar-default .navbar-nav>.open>a:focus{
-		color: #fff;
-		background-color: #18bc9c;
-	}
-	.navbar-default .navbar-nav>li:hover{
-		background-color: #18bc9c;
-	}
-	.navbar-default .navbar-nav>li>a:hover{
-		color: #ffffff;
-	}
-	.langoptions>li>a:hover, .langoptions>li>a:focus{
-		color: #fff;
-		background-color: #18bc9c;
-	}
-
-	.navbar-brand>img {
-		margin-top: -7px;
-    	vertical-align: baseline;
-	}
-	.navbar-brand>img:hover{
-		transform: scale(1.1, 1.1);
-	}
+body{
+	font-size: 16px;
+}	
+.container-fluid {
+    padding-right: 0px;
+    padding-left: 0px;
+}
 </style>
-
